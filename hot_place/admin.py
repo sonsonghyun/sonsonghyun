ec6a2b1f202f25bdc49a9607ba9297fc6d7194b7
sonsonghyun <98512998+sonsonghyun@users.noreply.github.com>
2022-04-16 10:11:49 +0900

    Delete hot_place directory

--- hot_place/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import Hot_place
-
-class Hot_placeAdmin(admin.ModelAdmin):
-
-    list_display = ['pk','title','contents','address','number','time','photo1']
-
-admin.site.register(Hot_place,Hot_placeAdmin)
--- hot_place/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HotPlaceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hot_place'
